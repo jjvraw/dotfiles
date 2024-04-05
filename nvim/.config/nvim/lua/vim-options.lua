75939fc12d365db1d3b5ea7afdadff95b623ab8a
Joshua James Venter <25530194@sun.ac.za>
2024-04-05 21:22:42 +0200

    vim options

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -17,4 +17,8 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-
+-- Delete to the black hole register by default
+-- when I delete, I dont want this to be saved.
+vim.keymap.set('n', 'd', '"_d')
+vim.keymap.set('n', 'dd', '"_dd')
+vim.keymap.set('v', 'd', '"_d')
