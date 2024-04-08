85976691bfacf0376db69e185b5eaecf27955732
Joshua James Venter <25530194@sun.ac.za>
2024-04-08 20:57:10 +0200

    google_format_java

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,52 +1,51 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"jdtls",
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "jdtls",
                     "pyright",
                     "clangd",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities
-			})
-			lspconfig.jdtls.setup({
-                capabilities = capabilities
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.jdtls.setup({
+                capabilities = capabilities,
             })
             lspconfig.pyright.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             lspconfig.clangd.setup({
                 capabilities = capabilities,
-                
             })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename)
-		end,
-	}
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename)
+        end,
+    },
 }
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -7,7 +7,12 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.clang_format,
-			},
+                null_ls.builtins.formatting.google_java_format.with({
+                    extra_args = function()
+                        return {"--tab-width", "4"}
+                    end
+                }),
+            },
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
