d31cb49e56efed46490805b4c59b1156896872f8
Joshua James Venter <25530194@sun.ac.za>
2024-04-08 21:40:09 +0200

    set tabsize 4 for java google format

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,21 +1,18 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.formatting.google_java_format.with({
-                    extra_args = function()
-                        return {"--tab-width", "4"}
-                    end
-                }),
-            },
-		})
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.google_java_format.with({
+          extra_args = { "--aosp", "--indent-size", "4" },
+        }),
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
