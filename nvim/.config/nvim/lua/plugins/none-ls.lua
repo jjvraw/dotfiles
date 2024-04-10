c897410bc39c220cba31ba189b4e9140a5ccb77b
Joshua James Venter <25530194@sun.ac.za>
2024-04-10 23:50:17 +0200

    java code convention

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,9 +6,11 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.clang_format.with({
+          filetypes = { "c", "cpp", "h" },
+        }),
         null_ls.builtins.formatting.google_java_format.with({
-          extra_args = { "--aosp", "--indent-size", "4" },
+          extra_args = {  "--aosp" },
         }),
       },
     })
