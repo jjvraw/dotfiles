65fc048eb23f0da94e6965a632f39f9d782fc77e
Joshua James Venter <25530194@sun.ac.za>
2024-04-24 23:35:54 +0200

    about time

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,20 +1,20 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.clang_format.with({
-          filetypes = { "c", "cpp", "h" },
-        }),
-        null_ls.builtins.formatting.google_java_format.with({
-          extra_args = {  "--aosp" },
-        }),
-      },
-    })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.clang_format.with({
+					filetypes = { "c", "cpp", "h" },
+				}),
+				null_ls.builtins.formatting.google_java_format.with({
+					extra_args = { "--aosp" },
+				}),
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
