6bda252d2b4f698e81e682c57e08a3824288e65a
Joshua James Venter <venter.joshua@gmail.com>
2025-05-29 07:46:13 +0200

    add editorconfig + remove column width marker

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,25 +16,31 @@ require("lazy").setup("plugins")
 
 -- For tmux auto window renaming
 if vim.env.TMUX then
-  vim.api.nvim_create_augroup("tmux", { clear = true })
-  vim.api.nvim_create_autocmd({"BufReadPost", "FileReadPost", "BufNewFile", "FocusGained"}, {
-    group = "tmux",
-    callback = function()
-      local filename = vim.fn.expand("%:t")
-      if filename == "" then
-        -- We're likely in a directory view (like netrw or oil.nvim)
-        local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-        vim.fn.system("tmux rename-window " .. vim.fn.shellescape(dir_name))
-      else
-        vim.fn.system("tmux rename-window " .. vim.fn.shellescape(filename))
-      end
-    end
-  })
-  vim.api.nvim_create_autocmd("VimLeave", {
-    group = "tmux",
-    callback = function()
-      local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-      vim.fn.system("tmux rename-window " .. vim.fn.shellescape(dir_name))
-    end
-  })
+    vim.api.nvim_create_augroup("tmux", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost", "BufNewFile", "FocusGained" }, {
+        group = "tmux",
+        callback = function()
+            local filename = vim.fn.expand("%:t")
+            if filename == "" then
+                -- We're likely in a directory view (like netrw or oil.nvim)
+                local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+                vim.fn.system("tmux rename-window " .. vim.fn.shellescape(dir_name))
+            else
+                vim.fn.system("tmux rename-window " .. vim.fn.shellescape(filename))
+            end
+        end
+    })
+    vim.api.nvim_create_autocmd("VimLeave", {
+        group = "tmux",
+        callback = function()
+            local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+            vim.fn.system("tmux rename-window " .. vim.fn.shellescape(dir_name))
+        end
+    })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "FileType", "ColorScheme" }, {
+        callback = function()
+            vim.cmd("set colorcolumn=")
+        end,
+    })
 end
