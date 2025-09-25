f6478d9ffddd5e8ab92c2726311aadd39b0ba05f
Joshua James Venter <venter.joshua@gmail.com>
2025-09-25 21:34:23 +0200

    pyright lsp:  pyright_python_path

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,18 +1,18 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format.with({
-					filetypes = { "c", "cpp", "h" },
-				}),
-				null_ls.builtins.formatting.google_java_format.with({
-					extra_args = { "--aosp" },
-				}),
-			},
-		})
-	end,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.clang_format.with({
+                    filetypes = { "c", "cpp", "h" },
+                }),
+                null_ls.builtins.formatting.google_java_format.with({
+                    extra_args = { "--aosp" },
+                }),
+            },
+        })
+    end,
 }
